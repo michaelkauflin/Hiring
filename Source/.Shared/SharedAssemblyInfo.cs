205fc71c1ed1cd049701ed8a438c5aea300da8c2
Philippe Morier <philippe.morier@phonak.com>
2015-02-03 15:58:11 +0100

    Add mandatory folder and files.

---
+++ Source/.Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+// Not used and set to empty string for all assemblies
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif // DEBUG
+
+[assembly: AssemblyCompany("Sonova")]
+[assembly: AssemblyProduct("YourComponentName")]
+[assembly: AssemblyCopyright("Copyright © Sonova 2015")]
