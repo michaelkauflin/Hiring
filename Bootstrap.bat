0f822d905cb3fc9e910c2588007bfff828715993
Franz.Schnyder <Franz.Schnyder@sonova.com>
2015-09-25 11:04:22 +0200

    Update to Bootstrapper 1.3.1

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -3,7 +3,7 @@ REM This script fetches the bootstrapper NuGet and passes control to the bootstr
 REM This is all this script should do, don't add other logic here. All working copy
 REM initialization should be carried out by the bootstrapper.
 
-nuget install Sonova.Bootstrapper -Version 1.2.1 -ExcludeVersion -OutputDirectory "%~dp0\Build" > con < con
+nuget install Sonova.Bootstrapper -Version 1.3.1 -ExcludeVersion -OutputDirectory "%~dp0\Build" > con < con
 call %~dp0\Build\Sonova.Bootstrapper\tools\Bootstrap.bat
 
 @ECHO on
