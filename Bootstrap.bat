28cf9b0222bf50dfcdbc540f7da0fc8096639bef
Vazquez, Miguel <miguel.vazquez@sonova.com>
2017-06-20 08:40:23 +0200

    Block paket to v4.8.8 in order to avoid breaking changes in paket.lock format until ToolBox supports it

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -3,7 +3,7 @@ REM This script fetches the bootstrapper NuGet and passes control to the bootstr
 REM This is all this script should do, don't add other logic here. All working copy
 REM initialization should be carried out by the bootstrapper.
 
-nuget install Sonova.Bootstrapper -Version 2.0.2 -Source https://artifacts.sonova.com/artifactory/api/nuget/components-production-local -ExcludeVersion -OutputDirectory "%~dp0\Build" > con < con
-call "%~dp0\Build\Sonova.Bootstrapper\tools\Bootstrap.bat" paket
+nuget install Sonova.Bootstrapper -Version 2.1.0 -Source https://artifacts.sonova.com/artifactory/api/nuget/components-production-local -ExcludeVersion -OutputDirectory "%~dp0\Build" > con < con
+call "%~dp0\Build\Sonova.Bootstrapper\tools\Bootstrap.bat" paket 4.8.8
 
 @ECHO on
