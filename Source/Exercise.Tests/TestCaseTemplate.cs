9696f14020e94737f8b47f8b2e08376c82609779
Marlet, Jules <jules.marlet@sonova.com>
2019-01-23 15:23:44 +0100

    Add TestCaseTemplate File for next interview session

---
+++ Source/Exercise.Tests/TestCaseTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise;
+
+using NUnit.Framework;
+
+namespace Exercise.Tests
+{
+    [TestFixture]
+    internal class TestCaseTemplate
+    {
+        [Test]
+        // First Unit Test
+        public void FirstUnitTest()
+        {
+
+        }
+
+    }
+}
