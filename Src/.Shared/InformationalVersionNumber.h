b8eae61f8041874fa6be069f9b4f91554b684186
Kauflin, Michael <Michael.Kauflin@sonova.com>
2020-06-15 08:59:45 +0200

    remove not needed stuff

--- Src/.Shared/InformationalVersionNumber.h
+++
@@ -1,3 +0,0 @@
-// This is a generated file, do not edit manually.
-
-#define STRPRODUCTVER "0.1.1-pre-u20180919-121133"
--- Src/.Shared/VersionInfo.g.cs
+++
@@ -1,4 +0,0 @@
-// This is a generated file, do not edit manually.
-
-[assembly: System.Reflection.AssemblyVersion("0.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.1.0.0")]
