ec8228564b1c44ea8d860d88160e0af1c06ca01f
Kauflin, Michael <Michael.Kauflin@sonova.com>
2020-06-15 08:53:31 +0200

    .NET SDK

---
+++ Src/.Shared/ReleaseVersionInfo.g.cs
@@ -0,0 +1,3 @@
+// This is a generated file, do not edit manually.
+
+[assembly: System.Reflection.AssemblyInformationalVersion("0.1.1-pre-u20180919-121133")]
--- ToolBox.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-
-cd "%~dp0\Build\packages\Sonova.ToolBox.Gui\tools"
-
-start "ToolBox" "Sonova.ToolBox.Gui.exe" -b "%~dp0 "
-
-@ECHO on
