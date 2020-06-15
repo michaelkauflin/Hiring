346d61a72c0d1b6bbff7e617c344587600276f60
Kauflin, Michael <Michael.Kauflin@sonova.com>
2020-06-15 09:06:26 +0200

    remove stuff

--- Bootstrap.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-REM This script fetches the bootstrapper NuGet and passes control to the bootstrapper.
-REM This is all this script should do, don't add other logic here. All working copy
-REM initialization should be carried out by the bootstrapper.
-
-nuget install Sonova.Bootstrapper -Version 2.1.0 -Source https://artifacts.sonova.com/artifactory/api/nuget/components-production-local -ExcludeVersion -OutputDirectory "%~dp0\Build" > con < con
-call "%~dp0\Build\Sonova.Bootstrapper\tools\Bootstrap.bat" paket
-
-@ECHO on
