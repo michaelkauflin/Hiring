754d18f89afd975d767cf22d2ffb7788c7000b57
Michael Kauflin <Michael.Kauflin@sonova.com>
2022-09-08 11:33:03 +0200

    change project to NUnitLite

---
+++ src/Exercise.Tests/Program.cs
@@ -0,0 +1,10 @@
+namespace Exercise.Tests
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new NUnitLite.AutoRun().Execute(args);
+        }
+    }
+}
